c72af6fdd2fc3eea817dfa46136f9dcbd2233a73
viktoriya_moiseeva <adxomega@yandex.ru>
2023-04-16 14:01:33 +0300

    meetupsApi

--- src/api/meetupsApi.js
+++ src/api/meetupsApi.js
@@ -23,7 +23,7 @@ export function getMeetup(id) {
  * @returns {Promise<ResultContainer<Meetup>>}
  */
 export function postMeetup(meetup) {
-  // TODO: реализовать функцию
+  return httpClient.post('/meetups', meetup);
 }
 
 /**
@@ -32,7 +32,7 @@ export function postMeetup(meetup) {
  * @returns {Promise<ResultContainer<Meetup>>}
  */
 export function putMeetup(meetup) {
-  // TODO: реализовать функцию
+  return httpClient.put(`/meetups/${meetup.id}`, meetup);
 }
 
 /**
@@ -41,7 +41,7 @@ export function putMeetup(meetup) {
  * @returns {Promise<ResultContainer<void>>}
  */
 export function deleteMeetup(id) {
-  // TODO: реализовать функцию
+  return httpClient.delete(`/meetups/${id}`);
 }
 
 /**
@@ -50,7 +50,7 @@ export function deleteMeetup(id) {
  * @returns {Promise<ResultContainer<void>>}
  */
 export function attendMeetup(id) {
-  // TODO: реализовать функцию
+  return httpClient.post(`/meetups/${id}/participation`)
 }
 
 /**
@@ -59,5 +59,5 @@ export function attendMeetup(id) {
  * @returns {Promise<ResultContainer<void>>}
  */
 export function leaveMeetup(id) {
-  // TODO: реализовать функцию
+  return httpClient.delete(`/meetups/${id}/participation`)
 }
