9ffec3373d03c5c3e30cd1412bdba1855db9ac12
viktoriya_moiseeva <adxomega@yandex.ru>
2023-04-16 22:40:12 +0300

    scrollBehaviour

--- src/router/scrollBehaviour.js
+++ src/router/scrollBehaviour.js
@@ -1,5 +1,3 @@
-// TODO: Task 05-vue-router/03-ScrollBehavior
-
 /** @implements {import('vue-router').RouterScrollBehavior} */
 export function scrollBehavior(to, from, savedPosition) {
   if (savedPosition) {
